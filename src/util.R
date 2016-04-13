5e0faac8f688b1abe8ec33a9dbc83233576628db
Xiang Zhu <xiangzhu.nku@gmail.com>
2016-04-13 17:03:10 -0500

    add util functions

---
+++ src/util.R
@@ -0,0 +1,46 @@
+#' normalize the log probabilities
+#' 
+#' @param logw a vector that contains the unnormalized log probabilities
+#' @return w a vector that contains the normalized probabilities
+#'  
+normalize.logw <- function(logw){
+  # guard against underflow or overflow
+  c <- max(logw)
+  w <- exp(logw-c)
+  # normalize the probabilities
+  w <- w / sum(w)
+  return(w)
+}
+
+#' convert gsea results in the mat file to a data frame
+#' 
+#' @param gsea.path a string the indicates the path of mat file 
+#' @return gsea.df a data frame that contains the results in the mat file 
+#' 
+gsea.mat2df <- function(gsea.path){
+  gsea.data <- R.matlab::readMat(gsea.path)
+  
+  gsea.df <- data.frame(
+    id <- c(gsea.data$path.id),
+    name <- unlist(gsea.data$path.name),
+    source <- unlist(gsea.data$path.sr),
+    database <- unlist(gsea.data$path.base),
+    numgene <- c(gsea.data$path.numg),
+    numsnps <- c(gsea.data$path.nums),
+    log10.bf <- log10(c(gsea.data$path.bf)),
+    theta.mean <- gsea.data$theta.est[, 1],
+    theta.95lb <- gsea.data$theta.est[, 3],
+    theta.95ub <- gsea.data$theta.est[, 4]
+  )
+  names(gsea.df) <- c("id","name","source","database","numgene","numsnps",
+                      "log10.bf","theta.mean","theta.95lb","theta.95ub")
+  
+  # convert all factor columns to characters
+  i <- sapply(gsea.df, is.factor)
+  gsea.df[i] <- lapply(gsea.df[i], as.character)
+  
+  # convert the names of pathways to ASCII
+  gsea.df$name <- iconv(gsea.df$name, 'UTF-8', 'ASCII')
+  
+  return(gsea.df)
+}
