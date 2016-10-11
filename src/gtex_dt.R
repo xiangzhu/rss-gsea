71e8ca78d4d9c92133cbf3a7ade9b7f01142e838
Xiang Zhu <xiangzhu.nku@gmail.com>
2016-10-11 12:59:25 -0500

    rmv filter from gtex table

--- src/gtex_dt.R
+++ src/gtex_dt.R
@@ -1,1 +1,1 @@
-gtex.dt <- DT::datatable(gtex.df,rownames=FALSE,class="display",filter="bottom",extensions="FixedColumns",options=list(dom='lftp',scrollX=TRUE,fixedColumns=TRUE)) %>% DT::formatRound(c("log10.bf","theta.mean","theta.95lb","theta.95ub"), 3)
+gtex.dt <- DT::datatable(gtex.df,rownames=FALSE,class="display",extensions="KeyTable",options=list(keys=TRUE)) %>% DT::formatRound(c("log10.bf","theta.mean","theta.95lb","theta.95ub"), 3)
