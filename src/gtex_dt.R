4748626644cf6802fab5188565c4b84b5be07494
xiangzhu <xiangzhu.nku@gmail.com>
2017-04-08 23:08:56 -0500

    rm unused files

--- src/gtex_dt.R
+++
@@ -1,1 +0,0 @@
-gtex.dt <- DT::datatable(gtex.df,rownames=FALSE,class="display",extensions="KeyTable",options=list(keys=TRUE)) %>% DT::formatRound(c("log10.bf","theta.mean","theta.95lb","theta.95ub"), 3)
